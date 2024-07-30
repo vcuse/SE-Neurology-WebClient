fddc7e5e31aa1ed5fea96d3e648bde05e54ae73b
Adam Hussein <adamhusse818@gmail.com>
2024-07-30 14:54:12 -0400

    Progress on account creation

--- loginPage/login.js
+++ loginPage/login.js
@@ -11,13 +11,45 @@ function submit(){
             method: "POST",
             body: JSON.stringify(data),
             headers: {
-                'Content-Type': 'application/json'
+                'Content-Type': 'application/json',
+                'Action': 'login'
             },
         })
         .then((response) => response.text())
         .then((result) => {
             window.alert(result);
             if(result === "ACCESS GRANTED"){
+                window.location.href = "../index.html?username=" + encodeURIComponent(username);
+            }
+        })
+        .catch((err) => console.log(err));
+    }
+}
+
+function create(){
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    if(username.length === 0 || password.length === 0){
+        window.alert("Please fill in all fields");
+    }
+    else{
+        const data = {username: username, password: password};
+        fetch("http://localhost:9000/key=peerjs/post", {
+            method: "POST",
+            body: JSON.stringify(data),
+            headers: {
+                'Content-Type': 'application/json',
+                'Action': 'create'
+            },
+        })
+        .then((response) => response.text())
+        .then((result) => {
+            if(result === "TAKEN"){
+                window.alert("Username is already in use.");
+            }
+            else{
+                window.alert("Account successfully created.");
                 window.location.href = "../index.html?username=" + encodeURIComponent(username);
             }
         })
