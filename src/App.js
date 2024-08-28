8d81654e79c47cf0763b31a7455e3f7ab724cb68
Grace Gillam <157736664+gillamga@users.noreply.github.com>
2024-08-28 03:57:04 +0000

    login page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
-
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route } from 'react-router-dom';
 import Login from "./pages/Login";
-import DoctorPage from "./pages/DoctorPage";
-import PatientPage from "./pages/PatientPage";
+
 
 function App() {
   return (
---
+++ src/pages/PatientPage.tsx
@@ -0,0 +1,7 @@
+// Basic placeholder component to avoid errors
+const PatientPage = () => {
+    return <div>Patient Page</div>;
+  };
+  
+  export default PatientPage;
+  
