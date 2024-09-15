313ee93ac6d10234ce610389239e43eeb6ca0668
samuelsarzaba <samuelsarzaba@gmail.com>
2024-09-15 18:55:22 -0400

    New login page without functionality

---
+++ src/components/auth/login-form.tsx
@@ -0,0 +1,79 @@
+"use client";
+
+import CardWrapper from "./card-wrapper";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { LoginSchema } from "@/schema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import { z } from "zod";
+import { useFormStatus } from "react-dom";
+import { useState } from "react";
+
+const LoginForm = () => {
+  const [loading, setLoading] = useState(false);
+  const form = useForm({
+    resolver: zodResolver(LoginSchema),
+    defaultValues: {
+      username: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = (data: z.infer<typeof LoginSchema>) => {
+    setLoading(true);
+    console.log(data);
+  };
+
+  const { pending } = useFormStatus();
+  return (
+    <CardWrapper label="Login to your account" title="Login">
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+          <div className="space-y-4">
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input {...field} type="password" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <Button type="submit" className="w-full" disabled={pending}>
+            {loading ? "Loading..." : "Login"}
+          </Button>
+        </form>
+      </Form>
+    </CardWrapper>
+  );
+};
+
+export default LoginForm;
