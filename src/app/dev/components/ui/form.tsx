e4457f8315c2188a980f90bfe8df8a026025b778
David <ferrufinoda2@vcu.edu>
2024-10-31 16:22:08 -0400

    tewts

--- src/app/dev/components/ui/form.tsx
+++ src/app/dev/components/ui/form.tsx
@@ -12,7 +12,7 @@ import {
   useFormContext,
 } from "react-hook-form"
 
-import { cn } from "@/app/lib/utils"
+import { cn } from "@/app/dev/lib/utils"
 import { Label } from "@/app/components/ui/label"
 
 const Form = FormProvider
