84fe966cb3723163c91afdf6966b173905497f16
samuelsarzaba <samuelsarzaba@gmail.com>
2024-11-14 16:56:35 -0500

    Added use client tag to appropriate components

--- src/components/stroke-scale/stroke-scale-form.tsx
+++ src/components/stroke-scale/stroke-scale-form.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import React, { useState } from 'react';
 import { Card } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
--- src/components/users/videoCall.tsx
+++ src/components/users/videoCall.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { useEffect, useState, useRef } from 'react';
 import Peer, { DataConnection, MediaConnection } from 'peerjs';
 
