8109c1636f726fb22f8fa8eaa19653941aa09cbd
David <ferrufinoda2@vcu.edu>
2025-05-05 11:22:13 -0400

    reverting change

--- src/hooks/usePeerConnection.tsx
+++ src/hooks/usePeerConnection.tsx
@@ -86,8 +86,8 @@ export function usePeerConnection() {
   useEffect(() => {
     const storedPeerId = localStorage.getItem('peerId');
     const peer = new Peer(storedPeerId || '', {
-      host: "devbranch-server-dot-videochat-signaling-app.ue.r.appspot.com",
-      port: 443,
+      host: "localhost",
+      port: 9000,
       secure: true,
       path: "/",
       debug: 3,
