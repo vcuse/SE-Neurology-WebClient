b1d8c31d73939d987c2cc41a3074294f81a62b2f
David <ferrufinoda2@vcu.edu>
2025-05-22 13:36:18 -0400

    local development now works!

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,22 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+    // ... other Next.js configs
+    publicRuntimeConfig: { // Available on both client and server, usually for small configs
+      someConfigValue: process.env.NEXT_PUBLIC_SOME_ENV_VAR,
+    },
+    webpack: (config, { isServer, buildId, dev, config: { distDir } }) => {
+      // Access your custom build target here
+      const BUILD_TARGET = process.env.BUILD_TARGET || 'development';
   
+      if (BUILD_TARGET === 'staging') {
+        console.log('Applying staging-specific webpack config...');
+        // e.g., add a specific plugin, or change a loader rule
+      }
+  
+      // Always return the modified config
+      return config;
+    },
 };
 
 export default nextConfig;
--- src/hooks/usePeerConnection.tsx
+++ src/hooks/usePeerConnection.tsx
@@ -84,11 +84,14 @@ export function usePeerConnection() {
   };
 
   useEffect(() => {
+    const secure = process.env.NEXT_PUBLIC_SERVER_SECURE; // This will be "true", "false", or undefined
+    console.log(`secure is ${secure}`)
+    const isSecure = secure == 'true';
     const storedPeerId = localStorage.getItem('peerId');
     const peer = new Peer(storedPeerId || '', {
-      host: "videochat-signaling-app.ue.r.appspot.com",
-      port: 443,
-      secure: true,
+      host: process.env.NEXT_PUBLIC_SERVER_URL!,
+      port: Number(process.env.NEXT_PUBLIC_SERVER_PORT),
+      secure: isSecure,
       path: "/",
       debug: 3, 
     });
@@ -120,7 +123,7 @@ export function usePeerConnection() {
 
     // Fetch peer IDs
     const fetchPeerIds = () => {
-      fetch("https://videochat-signaling-app.ue.r.appspot.com/key=peerjs/peers",{ credentials: 'include'}) 
+      fetch(process.env.NEXT_PUBLIC_SERVER_FETCH_PEERS!,{ credentials: 'include'}) 
         .then((response) => {
           if (!response.ok) {
             throw new Error("Failed to fetch peer IDs");
