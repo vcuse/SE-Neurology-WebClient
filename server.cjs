202dd64e0ca78f79936da2baa820bc2049459b09
David <ferrufinoda2@vcu.edu>
2025-11-03 12:30:43 -0500

    fixed server to let it be upgraded to https by nginx

--- server.cjs
+++ server.cjs
@@ -9,18 +9,18 @@ const dev = process.env.NODE_ENV;
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
-const httpsOptions = {
-    // ⚠️ UPDATE THESE PATHS to your certificate files
-    key: fs.readFileSync(path.join(__dirname, 'sslcerts', 'key.pem')),
-    cert: fs.readFileSync(path.join(__dirname, 'sslcerts', 'cert.pem'))
-};
+// const httpsOptions = {
+//     // ⚠️ UPDATE THESE PATHS to your certificate files
+//     key: fs.readFileSync(path.join(__dirname, 'sslcerts', 'key.pem')),
+//     cert: fs.readFileSync(path.join(__dirname, 'sslcerts', 'cert.pem'))
+// };
 
 app.prepare().then(() => {
-    createServer(httpsOptions, (req, res) => {
+    createServer( (req, res) => {
         const parsedUrl = parse(req.url, true);
         handle(req, res, parsedUrl);
     }).listen(port, (err) => {
         if (err) throw err;
-        console.log(`> Ready on https://localhost:${port}`);
+        console.log(`> Ready on http://localhost:${port}`);
     });
 }); 
