12372b995de17cc3540d47de64df0ced575983ec
David <ferrufinoda2@vcu.edu>
2025-11-03 19:24:40 -0500

    changes for local debug

--- server.cjs
+++ server.cjs
@@ -21,6 +21,6 @@ app.prepare().then(() => {
         handle(req, res, parsedUrl);
     }).listen(port, (err) => {
         if (err) throw err;
-        console.log(`> Ready on http://localhost:${port}`);
+        console.log(`> Ready on http://localhost:${port}/b/login`);
     });
 }); 
